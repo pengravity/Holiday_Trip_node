df7e10ee26fa22e1fcc52407cc8a87584bfb6561
Andy <alipinski@mail.com>
2022-12-04 12:31:20 +0100

    GET tours data

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from the server' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(200).json({ message: 'You can post to this endpoint...' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`app running on port ${port}...`);
+});
