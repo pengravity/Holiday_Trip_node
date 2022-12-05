18f8f99c5d022399b76a597f6d5570883e167833
Andy <alipinski@mail.com>
2022-12-05 21:03:53 +0100

    eslint and prettier configuration

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const userRouter = require('./routes/userRoutes.js');
 const app = express();
 
 // middlewares
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
+
+// serve static files
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
 // START SERVER
-const port = 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
