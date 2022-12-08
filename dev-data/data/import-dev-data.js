c78bb75171371488301c1a4d27ac84824fd31a3c
Andy <alipinski@mail.com>
2022-12-08 18:45:33 +0100

    script to delete collection from mongoDB OR to load data from JSON file

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_Password);
+// console.log(DB);
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB).then((con) => {
+  console.log('DB connected!');
+});
+
+// Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// delete all data from db
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
