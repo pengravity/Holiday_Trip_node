2f83695a2d3d0ababe3d1caada5c4b7b51f980e1
Andy <alipinski@mail.com>
2022-12-09 13:33:00 +0100

    query pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -44,6 +44,17 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // execute query
     const tours = await query;
 
