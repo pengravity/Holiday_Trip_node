38293d72b3b60a12215dad01e48e85d262291963
Andy <alipinski@mail.com>
2022-12-10 15:57:08 +0100

    send different error info depending if it is dev or prod env, send different error in case of isOperational error in prod,

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,39 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // operational known errors -> send message to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // some other error(programming or unknown) -> dont send the details to the client
+  } else {
+    // log error
+    console.log('ERROR !!!', err);
+
+    // send a generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong...',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev();
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd();
+  }
 };
