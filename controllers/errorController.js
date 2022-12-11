f81741849d415347e59270bc49a2fc2752154366
Andy <alipinski@mail.com>
2022-12-11 18:47:47 +0100

    code cleanup

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,4 @@
-const sendErrorDev = (err, res) => {
+const sendError = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
@@ -7,33 +7,9 @@ const sendErrorDev = (err, res) => {
   });
 };
 
-const sendErrorProd = (err, res) => {
-  // operational known errors -> send message to the client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-    // some other error(programming or unknown) -> dont send the details to the client
-  } else {
-    // log error
-    console.log('ERROR !!!', err);
-
-    // send a generic message
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went wrong...',
-    });
-  }
-};
-
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorDev();
-  } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd();
-  }
+  sendError(err, res);
 };
