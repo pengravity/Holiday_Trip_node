872eaced09d48ee2209df015f5003022ef9e549a
Andy <alipinski@mail.com>
2022-12-18 21:17:30 +0100

    data sanitization against noSQL query injection and XSS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -35,6 +37,13 @@ app.use(
     limit: '10kb',
   })
 );
+
+// data sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization against XSS
+app.use(xss());
+
 // serve static files
 app.use(express.static(`${__dirname}/public`));
 
