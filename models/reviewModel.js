e04fb2922dd40c1f470d97eb59d5ade55c60923b
Andy <alipinski@mail.com>
2022-12-29 19:04:26 +0100

    virtual populate tours with reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -36,10 +36,10 @@ const reviewSchema = new mongoose.Schema(
 );
 
 reviewSchema.pre(/^find/, function (next) {
-  this.populate({
-    path: 'tour',
-    select: 'name',
-  });
+  //   this.populate({
+  //     path: 'tour',
+  //     select: 'name',
+  //   });
   this.populate({
     path: 'user',
     select: 'name photo',
